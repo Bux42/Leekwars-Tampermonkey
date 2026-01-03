2def5624491fc3d84c02a7e63257c236042e79cb
Bux42 <roszwoka2@gmail.com>
2026-01-03 16:59:07 +0100

    Add license for GreasyFork

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 // @match        https://leekwars.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=leekwars.com
 // @grant        none
+// @license MIT
 // ==/UserScript==
 
 const componentNamesToIds = {
